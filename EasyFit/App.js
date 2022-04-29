b0680f3e3195dc37a6f8ee01e76e47adf660f8bd
gpham2 <giangphamgia03@gmail.com>
2022-04-28 19:46:40 -0700

    zoomable image

--- EasyFit/App.js
+++ EasyFit/App.js
@@ -1,10 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ZoomableImage from './ZoomableImage';
+import { LogBox } from 'react-native';
+
+
+
+const annotations = [
+	{
+		x1: 25,
+		x2: 35,
+		y1: 20,
+		y2: 30,
+		description: 'A pair of black running sports shoes, has lace-up detail. Textile and mesh upper',
+	},
+	{
+		x1: 60,
+		x2: 70,
+		y1: 15,
+		y2: 25,
+		description: 'Shoe sole tip!',
+	},
+	{
+		x1: 20,
+		x2: 30,
+		y1: 50,
+		y2: 60,
+		description: 'Textured and patterned outsole',
+	},
+	{
+		x1: 65,
+		x2: 75,
+		y1: 65,
+		y2: 75,
+		description: 'Textured outsole with a stacked heel',
+	},
+]
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Workout App, initial setup done?!</Text>
+
+      <ZoomableImage
+					source={require('./public/front-view-test.jpg')}
+					imageHeight={ 600 }
+					imageWidth={ 450 }
+					annotations={annotations}
+					popOverStyles={ { backgroundColor: 'white' } }
+      />
+
+
       <StatusBar style="auto" />
     </View>
   );
